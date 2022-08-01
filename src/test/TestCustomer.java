0bd332b90e6a53993c3573785cfc0118c894d8c7
Adrian Bauer <adrian.go@ruralsourcing.com>
2022-08-01 11:55:50 -0400

    Created Test Cases for Customer and RandomGen

---
+++ src/test/TestCustomer.java
@@ -0,0 +1,117 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCustomer {
+	@Test
+	void testCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCustomerStringStringStringStringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRandomIdRead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsUnique() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDisplayHeader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPhoneNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPhoneNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAge() {
+		fail("Not yet implemented");
+	}
+
+}
